2a0973d6ec725ad201441744d8ac7a3a107154b2
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-07-31 19:58:42 +0900

    Implement XMLHTTPRequest

---
+++ popup/popup.js
@@ -0,0 +1,29 @@
+var sendRequest = function(i) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://lgtm.camph.net/random.json', true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      var data = JSON.parse(xhr.responseText);
+      var image = document.getElementById('image-' + i);
+      image.setAttribute('src', data.image_file);
+      image.addEventListener('click', function() {
+        chrome.tabs.sendMessage(tabId, {markdown: data.markdown}, function(response) {});
+      });
+    }
+  };
+  xhr.send();
+};
+
+var load = function() {
+  var tabId = 0;
+  chrome.tabs.query({active: true}, function(tab){
+    tabId = tab[0].id;
+  });
+  for (var i = 0; i < 3; i++) {
+    sendRequest(i);
+  }
+};
+
+window.onload = function() {
+  load();
+};
